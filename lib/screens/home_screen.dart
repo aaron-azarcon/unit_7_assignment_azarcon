5370d49c2100a1ebc0ef7ce8d61ec222d198f4a8
aaron <127000351+aaron-azarcon@users.noreply.github.com>
2024-10-30 12:54:29 +0800

    Add Home Screen View

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,24 +1,51 @@
 import 'package:flutter/material.dart';
 
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     backgroundColor: Color(0xFF082851),
       appBar: AppBar(
-        title: Text("Gesture and Navigation"),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("This is supposed to be a Home Screen"),
-          SizedBox(height: 20),
-          
-          // INSERT CODE BELOW
-        ],
-      ),
+        backgroundColor: Color(0xFF082851),
+        title:
+          const Center(
+            child: Text(
+                "Gesture and Navigation",
+                style: TextStyle(
+                    color: Color(0xFF11D9A6),
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold
+                ),
+              )
+          )),
+      body: Center(
+        child:
+          ElevatedButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/about');
+            },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(120, 55),
+                backgroundColor: Color(0xFF11D9A6),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25),
+                  side: BorderSide.none,
+                ),
+              ),
+            child:
+              const Text(
+                'Go to About Me',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF082851),
+                )
+              )
+          )
+      )
     );
   }
 }
